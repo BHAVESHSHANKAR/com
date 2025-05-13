2acf78c8dc95c7dce57cbfd5c7830d42afbf5a9c
BHAVESH <kallurbhavesh@gmail.com>
2025-05-13 16:13:31 +0530

    pushing files

--- server/server.js
+++ server/server.js
@@ -8,7 +8,7 @@ const PORT = process.env.PORT || 5000;
 
 // Middleware
 app.use(cors({
-    origin: 'http://localhost:5173',
+    origin: 'https://com-clients.vercel.app',
     methods: ['GET', 'POST', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization'],
     credentials: true
