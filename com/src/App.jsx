bdb4cffcb05f3aac5a42506f7e00d99869da248f
BHAVESH <kallurbhavesh@gmail.com>
2025-05-13 16:09:51 +0530

    pushing files

--- com/src/App.jsx
+++ com/src/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import {BrowserRouter,Routes,Route} from 'react-router-dom';
-import Mainpage from './pages/mainpage';
+import Mainpage from './pages/mainPage';
 
 function App(){
   return(
